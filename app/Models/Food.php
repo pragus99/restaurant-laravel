2f7939e6c7ea9c8fb57c5b9f4f886a7fb4af8a52
pragus99 <prayogo0399@gmail.com>
2021-01-18 19:24:57 +0700

    fix controller

--- app/Models/Food.php
+++ app/Models/Food.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Food extends Model
 {
@@ -13,7 +14,7 @@ class Food extends Model
 
     public function category()
     {
-        return $this->hasOne(category::class, 'id', 'category_id');
+        return $this->hasOne(Category::class, 'id', 'category_id');
     }
 
     public function takeImage()
